1aae65cb93dbb47e5e83bc4874e882a1b78af870
root <root@Estephany.localdomain>
2026-02-28 00:42:21 -0500

    Environment setup with PostgreSQL, Tailwind, necessary gems, and initial header design

---
+++ config/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ]
+}
