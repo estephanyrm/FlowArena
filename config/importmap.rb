243ef4ad78133743b3224b1c7713f1fa68864eda
root <root@Estephany.localdomain>
2026-03-08 20:40:19 -0500

    Error messages were implemented and the code was documented

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,4 @@ pin "el-transition" # @0.0.7
 pin "flowbite", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.turbo.min.js"
 pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
 pin "flowbite-datepicker" # @2.0.0
+pin "sweetalert2" # @11.26.20
